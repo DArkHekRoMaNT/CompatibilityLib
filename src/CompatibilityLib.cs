58c44913a6670b5c86b221d6593fb22293d749b7
DArkHekRoMaNT <darkhekromant@yandex.ru>
2021-02-11 20:32:06 +0300

    Add warning about shapes and textures

--- src/CompatibilityLib.cs
+++ src/CompatibilityLib.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Vintagestory.API.Common;
 
 namespace CompatibilityLib
@@ -5,10 +6,14 @@ namespace CompatibilityLib
     public class CompatibilityLib : ModSystem
     {
         public static AssetCategory compatibility = new AssetCategory("compatibility", true, EnumAppSide.Universal);
+
+        public static string[] partiallyWorkingCategories = { "shapes", "textures" };
+
         public override double ExecuteOrder()
         {
             return 0.04; //load before json patching
         }
+
         public override void Start(ICoreAPI api)
         {
             base.Start(api);
@@ -24,6 +29,15 @@ namespace CompatibilityLib
                     if (api.Assets.AllAssets.ContainsKey(newLoc)) api.Assets.AllAssets.Remove(newLoc);
 
                     asset.Location.Path = newLoc.Path;
+
+                    //Warning about partially worked categories
+                    if (partiallyWorkingCategories.Contains(asset.Location.Category.Code))
+                    {
+                        api.World.Logger.Warning("Asset categories: {0} - may work strange. " +
+                            "It is best to leave such assets out of the compatibility category!",
+                            string.Join(", ", partiallyWorkingCategories)
+                        );
+                    }
                 }
             }
         }
